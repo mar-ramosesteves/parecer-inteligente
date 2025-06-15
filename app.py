4a5f5eb19a4faca860096d743f477e5f3417dc93
mar-ramosesteves <mar.ramosesteves@gmail.com>
2025-06-14 22:54:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-CORS(app, supports_credentials=True, origins="*")
-
 
 app = Flask(__name__)
-CORS(app, origins=["https://gestor.thehrkey.tech"])
+CORS(app, supports_credentials=True, origins=["https://gestor.thehrkey.tech"])
+
 
 @app.route("/")
 def index():
