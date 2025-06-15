bc6413fadd365e95a079b3683bcc29a4ffed305b
mar-ramosesteves <mar.ramosesteves@gmail.com>
2025-06-14 21:34:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def index():
+    return "API no ar! 🚀"
+
+if __name__ == "__main__":
+    app.run()
 
