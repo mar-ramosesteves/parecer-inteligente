4054a779177e1d70c71b12f0747f82682fd97e80
mar-ramosesteves <mar.ramosesteves@gmail.com>
2025-07-31 13:49:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ def emitir_parecer_microambiente():
         }
 
         # 🧠 Salvar parecer
-        salvar_relatorio_no_supabase(dados_retorno, empresa, rodada, email_lider, tipo_relatorio)
+        salvar_relatorio_analitico_no_supabase(dados_retorno, empresa, rodada, email_lider, tipo_relatorio)
 
         response = jsonify(dados_retorno)
         response.headers["Access-Control-Allow-Origin"] = "https://gestor.thehrkey.tech"
